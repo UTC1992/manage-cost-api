6d4a38f808d7620a23feaa8147f92f80f81b8f1f
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2023-09-13 08:52:13 -0500

    the swagger was configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './modules/auth/controllers/auth.controller';
+import { MongooseConfigModule } from './modules/mongoose/mongoose.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [MongooseConfigModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar títulos de documentación
+  const options = new DocumentBuilder()
+    .setTitle('Manage Cost REST API')
+    .setDescription('API REST for control cost')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // La ruta en que se sirve la documentación
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
