ec2b717acd93c070231fe23b157ef59c48f7b3bb
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2023-09-13 14:07:43 -0500

    the field for swagger were added

--- src/modules/auth/dto/loginRequest.dto.ts
+++ src/modules/auth/dto/loginRequest.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class LoginRequestDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @MinLength(8)
   @MaxLength(16)
   @IsNotEmpty()
--- src/modules/user/dto/userRequest.dto.ts
+++ src/modules/user/dto/userRequest.dto.ts
@@ -1,17 +1,21 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { LoginRequestDto } from 'src/modules/auth/dto/loginRequest.dto';
 
 export class UserRequestDto extends PartialType(LoginRequestDto) {
+  @ApiProperty()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   dni: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   phone: string;
 }
